7ab1e9885c7eb98428b28260813b6d5fa9b798c8
joma-doma <romas.ussr@gmail.com>
2017-03-11 00:37:31 +0300

    Comments added

---
+++ AgeInDays/AgeInDays.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	puts("Enter your age in this year");
+	int age;
+	scanf("%d", &age);
+
+	time_t rawtime;
+	struct tm * timeinfo;
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	int days = 0;
+	for (int i = 0; i < age; i++)
+	{
+		days += 365;
+		if ((timeinfo->tm_year - i) % 4 == 0)
+			days++;
+	}
+
+	printf("Days in life %d\n", days);
+
+	return 0;
+}
